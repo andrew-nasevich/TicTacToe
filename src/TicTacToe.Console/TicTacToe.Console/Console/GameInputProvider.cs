5d3970d4bcbed5a8f11bcee45d232121521a8c60
Andrew <nasevichandrey@gmail.com>
2019-12-16 19:51:56 +0300

    Console app bootstrap was implemented

---
+++ src/TicTacToe.Console/TicTacToe.Console/Console/GameInputProvider.cs
@@ -0,0 +1,24 @@
+﻿using TicTacToe.Console.Interfaces;
+using TicTacToe.Foundation.Interfaces;
+
+namespace TicTacToe.Console.Console
+{
+    public class GameInputProvider : IGameInputProvider
+    {
+        private readonly IConsole _console;
+
+
+        public GameInputProvider(IConsole console)
+        {
+            _console = console;
+        }
+
+
+        public void GetNextCellPosition(out int row, out int column, IPlayer player)
+        {
+            _console.WriteLine($"{player.Name}, please, write your next figure position");
+            row = _console.ReadInt("Please, write row: ") - 1;
+            column = _console.ReadInt("Please, write column: ") - 1;
+        }
+    }
+}
--- src/TicTacToe/Program.cs
+++ src/TicTacToe/Program.cs
@@ -1,15 +1,117 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TicTacToe.Console.Boards;
+using TicTacToe.Console.Interfaces;
+using TicTacToe.Console.Console;
+using TicTacToe.Console.Figures;
+using TicTacToe.Console.GameConfigurations;
+using TicTacToe.Console.Players;
+using TicTacToe.Foundation.Boards;
+using TicTacToe.Foundation.Cells;
+using TicTacToe.Foundation.Figures;
+using TicTacToe.Foundation.GameConfigurations;
+using TicTacToe.Foundation.Games;
+using TicTacToe.Foundation.Games.GameResults;
+using TicTacToe.Foundation.Games.StepResults;
+using TicTacToe.Foundation.Interfaces;
+using TicTacToe.Foundation.Players;
+using TicTacToe.Foundation.WinningStates;
 
 namespace TicTacToe
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static IConsole _console;
+        private static IBoardDrawer _boardDrawer;
+
+        public static void Main(string[] args)
         {
+            _console = new GameConsole();
+
+            var figureDrawerFactory = new FigureDrawerFactory(_console);
+            var figureDrawerProvider = new FigureDrawerProvider(figureDrawerFactory); 
+            _boardDrawer = new BoardDrawer(figureDrawerProvider, _console);
+
+            var gameInputProvider = new GameInputProvider(_console);
+            var playerFactory = new PlayerFactory();
+
+            var playerRegistrationService = new PlayerRegistrationService(playerFactory, _console);
+            var gameConfigurationFactory = new GameConfigurationFactory();
+            var gameConfigurationService = new GameConfigurationService(playerRegistrationService, _console, gameConfigurationFactory);
+
+            var cellFactory = new CellFactory();
+            var figureFactory = new FigureFactory();
+            var boardFactory = new BoardFactory(cellFactory, figureFactory);
+
+            var winningStateFactory = new WinningStateFactory();
+
+            IGameConfiguration gameConfiguration = null;
+            int chosenOption;
+            do
+            {
+                gameConfiguration = gameConfigurationService.GetGameConfiguration(gameConfiguration);
+
+                var game = new Game(gameInputProvider, gameConfiguration, boardFactory, winningStateFactory);
+                game.GameStepCompleted += OnGameStepCompleted;
+                game.GameFinished += OnGameFinished;
+
+                game.Run();
+                
+                _console.WriteLine("Next possible options:");
+                _console.WriteLine("1) Start new game with new players");
+                _console.WriteLine("2) Start new game with the same players");
+                _console.WriteLine("3) Exit");
+
+                do
+                {
+                    chosenOption = _console.ReadInt("Please, choose an option: ");
+                } while (chosenOption <= 0 || chosenOption > 3);
+
+                if (chosenOption == 1)
+                {
+                    gameConfiguration = null;
+                }
+                
+            } while (chosenOption != 3);
+        }
+
+
+        private static void OnGameFinished(object sender, GameFinishedEventArgs e)
+        {
+            var gameResult = e.GameResult;
+            switch (gameResult.GameResultType)
+            {
+                case GameResultType.Draw:
+                    _console.WriteLine("Friendship won!");
+                    break;
+                case GameResultType.Win:
+                    var winResult = (WinGameResult)gameResult;
+                    _console.WriteLine($"Player {winResult.WonPlayer.Name} won!");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException($"Unknown GameResultType value: {gameResult.GameResultType}");
+            }
+        }
+
+        private static void OnGameStepCompleted(object sender, GameStepCompletedEventArgs e)
+        {
+            var stepResult = e.StepResult;
+            switch (stepResult.StepResultType)
+            {
+                case StepResultType.Success:
+                    var successResult = (SuccessStepResult)stepResult;
+                    _boardDrawer.DrawBoard(successResult.Board);
+                    break;
+                case StepResultType.CellIsAlreadyFilled:
+                    var filledCellResult = (CellIsAlreadyFilledStepResult)stepResult;
+                    _console.WriteLine($"This cell is already filled with figure: {filledCellResult.Cell.Figure.Type}");
+                    break;
+                case StepResultType.InvalidCellPosition:
+                    var invalidResult = (InvalidCellPositionStepResult)stepResult;
+                    _console.WriteLine($"This is no cell at position: ({invalidResult.SelectedColumn},{invalidResult.SelectedRow})");
+                    break;
+                default:
+                    throw  new ArgumentOutOfRangeException($"Unknown StepResultType value: {stepResult.StepResultType}");
+            }
         }
     }
 }

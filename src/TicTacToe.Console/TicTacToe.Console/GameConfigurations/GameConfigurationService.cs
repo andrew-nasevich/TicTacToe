bcb00b335cd871ab54b2a122efd033107e088861
andrew-nasevich <49556147+andrew-nasevich@users.noreply.github.com>
2019-12-16 14:42:07 +0300

    GameConfigurationService implementation (#2)

---
+++ src/TicTacToe.Console/TicTacToe.Console/GameConfigurations/GameConfigurationService.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TicTacToe.Common.EnumerableExtensions;
+using TicTacToe.Console.Interfaces;
+using TicTacToe.Foundation.Figures;
+using TicTacToe.Foundation.Interfaces;
+
+namespace TicTacToe.Console.GameConfigurations
+{
+    public class GameConfigurationService : IGameConfigurationService
+    {
+        private readonly IPlayerRegistrationService _playerRegistrationService;
+        private readonly IConsole _console;
+        private readonly IGameConfigurationFactory _gameConfigurationFactory;
+
+
+        public GameConfigurationService(
+            IPlayerRegistrationService playerRegistrationService, 
+            IConsole console,
+            IGameConfigurationFactory gameConfigurationFactory)
+        {
+            _playerRegistrationService = playerRegistrationService;
+            _console = console;
+            _gameConfigurationFactory = gameConfigurationFactory;
+        }
+
+
+        public IGameConfiguration GetGameConfiguration(IGameConfiguration existingGameConfiguration)
+        {
+            var boardSize = GetBoardSize();
+            var players = existingGameConfiguration != null ? existingGameConfiguration.Players : GetPlayers();
+            var firstStepPlayer = GetFirstStepPlayer(players);
+
+            return _gameConfigurationFactory.CreateGameConfiguration(boardSize, players, firstStepPlayer);
+        }
+
+
+        private int GetBoardSize()
+        {
+            int boardSize;
+            do
+            {
+                boardSize = _console.ReadInt("Please, write board size: ");
+            } while (boardSize <= 1);
+
+            return boardSize;
+        }
+
+        private IReadOnlyCollection<IPlayer> GetPlayers()
+        {
+            var availableFigures = Enum.GetValues(typeof(FigureType)).Cast<FigureType>().ToList();
+            var amountOfPlayers = GetAmountOfPlayers(availableFigures.Count);
+            var players = new List<IPlayer>();
+
+            for (var i = 0; i < amountOfPlayers; i++)
+            {
+                var player = _playerRegistrationService.RegisterPlayer(availableFigures);
+                players.Add(player);
+                availableFigures.Remove(player.FigureType);
+                _console.WriteLine($"Player {player.Name} was registered.");
+            }
+
+            return players;
+        }
+
+        private int GetAmountOfPlayers(int amountOfFigureTypes)
+        {
+            int amountOfPlayers;
+            do
+            {
+                amountOfPlayers = _console.ReadInt("Please, write amount of players: ");
+            } while (amountOfPlayers <= 1 || amountOfPlayers > amountOfFigureTypes);
+
+            return amountOfPlayers;
+        }
+
+        private IPlayer GetFirstStepPlayer(IReadOnlyCollection<IPlayer> players)
+        {
+            _console.WriteLine("Players:");
+            players.ForEach((p, i) => _console.WriteLine($"{i + 1}) {p.Name}"));
+
+            int chosenNumberOfPlayer;
+            do
+            {
+                chosenNumberOfPlayer = _console.ReadInt("Please, white the number of the player who makes first step:");
+            } while (chosenNumberOfPlayer <= 0 || chosenNumberOfPlayer > players.Count);
+
+            return players.ElementAt(chosenNumberOfPlayer - 1);
+        }
+    }
+}
---
+++ src/TicTacToe.Console/TicTacToe.Console/Interfaces/IGameConfigurationService.cs
@@ -0,0 +1,9 @@
+﻿using TicTacToe.Foundation.Interfaces;
+
+namespace TicTacToe.Console.Interfaces
+{
+    public interface IGameConfigurationService
+    {
+        IGameConfiguration GetGameConfiguration();
+    }
+}
